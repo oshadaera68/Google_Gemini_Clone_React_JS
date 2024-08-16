117291c2bac0b276825999fafa3a46179074c17c
oshadaera68 <mindartlk010@gmail.com>
2024-08-16 16:08:41 +0530

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,56 @@
+import { assets } from "../../assets/assets";
+import "./main.css";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Dev.</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activites for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a Prompt Here" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
